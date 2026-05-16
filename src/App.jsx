ce1d811bb03d91e8fbad3571bdc4694d2a7e71dc
Risa <awbelajar@gmail.com>
2026-05-16 09:00:08 +0700

    hapus tombol import

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ export default function App() {
 
   const [validatedStudent, setValidatedStudent] =
     useState(null);
+  const [selectedCandidate, setSelectedCandidate] = useState(null);
 
   const [results, setResults] = useState({
     1: 0,
